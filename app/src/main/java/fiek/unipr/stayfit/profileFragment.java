1a4f5d78a296f27d91db77173285bb58371aafd5
Ali Tafaj <alittafaj@gmail.com>
2021-06-25 19:56:40 +0200

    Added access to the camera from profileFragment

--- app/src/main/java/fiek/unipr/stayfit/profileFragment.java
+++ app/src/main/java/fiek/unipr/stayfit/profileFragment.java
@@ -8,11 +8,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
@@ -23,6 +30,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 import static android.media.MediaRecorder.VideoSource.CAMERA;
 import static androidx.core.content.ContextCompat.checkSelfPermission;
 
@@ -31,6 +43,8 @@ public class profileFragment extends Fragment {
     ImageView iv_capture;
     View view;
     TextView tvProfileName;
+
+    private static final int Image_Capture_Code = 1;
 
     public profileFragment() {
         // Required empty public constructor
@@ -51,10 +65,11 @@ public class profileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //
+
         view = inflater.inflate(R.layout.fragment_profile, container, false);
-        captureBtn = (Button) view.findViewById(R.id.captureBtn);
-        iv_capture = getActivity().findViewById(R.id.iv_capture);
+        captureBtn = (Button)view.findViewById(R.id.captureBtn);
+        //iv_capture = getActivity().findViewById(R.id.iv_capture);
+        iv_capture = (ImageView)view.findViewById(R.id.iv_capture);
 
         Bundle args = getArguments();
         try {
@@ -64,17 +79,61 @@ public class profileFragment extends Fragment {
         }
         catch (Exception e){
             Toast.makeText(getActivity(),"empty arg", Toast.LENGTH_LONG).show();
+            //askCameraPremission();
         }
 
         captureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_LONG).show();
+                Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cInt,Image_Capture_Code);
+                //Toast.makeText(getActivity(), "Camera button is clicked", Toast.LENGTH_LONG).show();
+                //askCameraPremission();
             }
         });
 
         return view;
 
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Image_Capture_Code) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                iv_capture.setImageBitmap(bp);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+/*
+    private void askCameraPremission() {
+        if(ContextCompat.checkSelfPermission(this.getActivity(),Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this.getActivity(),new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+
+        }else {
+            openCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull  int[] grantResults) {
+
+        if(requestCode == CAMERA_PERM_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openCamera();
+            }else {
+                Toast.makeText(getActivity(), "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void openCamera() {
+        Toast.makeText(getActivity(), "Camera open request", Toast.LENGTH_SHORT).show();
+
+    }*/
 
 }
