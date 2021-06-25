5a7a9a4703262eb3534a2ad7599628e997ee2873
Ali Tafaj <alittafaj@gmail.com>
2021-06-25 15:40:50 +0200

    Updated designs on Register and Login Activity, Updated profileFragment

--- app/src/main/java/fiek/unipr/stayfit/MainActivity.java
+++ app/src/main/java/fiek/unipr/stayfit/MainActivity.java
@@ -1,3 +1,4 @@
+/*
 package fiek.unipr.stayfit;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}*/
+package fiek.unipr.stayfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        profileFragment fragment = new profileFragment();
+        Bundle args = new Bundle();
+        args.putString("email", getIntent().getExtras().getString("email"));
+        fragment.setArguments(args);
+
+
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+        fragmentTransaction.add(R.id.tvProfileName, fragment);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
--- app/src/main/java/fiek/unipr/stayfit/profileFragment.java
+++ app/src/main/java/fiek/unipr/stayfit/profileFragment.java
@@ -1,51 +1,114 @@
+/*
 package fiek.unipr.stayfit;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+import static androidx.core.content.ContextCompat.checkSelfPermission;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link profileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class profileFragment extends Fragment {
-
-    TextView tvName, tvLast;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button captureBtn;
+    ImageView iv_capture;
+    View view;
 
     public profileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment profileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static profileFragment newInstance(String param1, String param2) {
         profileFragment fragment = new profileFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        captureBtn = (Button) view.findViewById(R.id.captureBtn);
+        iv_capture = getActivity().findViewById(R.id.iv_capture);
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+
+    }
+
+}*//*
+
+package fiek.unipr.stayfit;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+import static androidx.core.content.ContextCompat.checkSelfPermission;
+
+public class profileFragment extends Fragment {
+    Button captureBtn;
+    ImageView iv_capture;
+    View view;
+    TextView tvProfileName;
+
+    public profileFragment() {
+        // Required empty public constructor
+    }
+
+    public static profileFragment newInstance(String param1, String param2) {
+        profileFragment fragment = new profileFragment();
+        Bundle args = new Bundle();
         return fragment;
     }
 
@@ -53,29 +116,116 @@ public class profileFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
-        DatabaseHelper db = new DatabaseHelper(getActivity());
-        DbResponse response = db.getData();
-/*
-        String email = getActivity().getIntent().getExtras().getString("email");
-        LoginActivity loginActivity = new LoginActivity();
-        loginActivity.LoginUser(email);*/
+        //
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        captureBtn = (Button) view.findViewById(R.id.captureBtn);
+        iv_capture = getActivity().findViewById(R.id.iv_capture);
 
-        tvName = rootView.findViewById(R.id.tvProfileName);
-        tvName.setText(response.name);
-        tvLast = rootView.findViewById(R.id.tvProfileLastName);
-        tvLast.setText(response.lastName);
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_profile, container, false);
-        return rootView;
+        Bundle args = getArguments();
+        try {
+            String myString = args.getString("email");
+            tvProfileName.setText(myString);
+
+        }
+        catch (Exception e){
+            Toast.makeText(getActivity(),"empty arg", Toast.LENGTH_LONG).show();
+        }
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+
     }
+
+}
+*/
+package fiek.unipr.stayfit;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+import static androidx.core.content.ContextCompat.checkSelfPermission;
+
+public class profileFragment extends Fragment {
+    Button captureBtn;
+    ImageView iv_capture;
+    View view;
+    TextView tvProfileName;
+
+    public profileFragment() {
+        // Required empty public constructor
+    }
+
+    public static profileFragment newInstance(String param1, String param2) {
+        profileFragment fragment = new profileFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        captureBtn = (Button) view.findViewById(R.id.captureBtn);
+        iv_capture = getActivity().findViewById(R.id.iv_capture);
+
+        Bundle args = getArguments();
+        try {
+            String myString = args.getString("email");
+            tvProfileName.setText(myString);
+
+        }
+        catch (Exception e){
+            Toast.makeText(getActivity(),"empty arg", Toast.LENGTH_LONG).show();
+        }
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+
+    }
+
 }
