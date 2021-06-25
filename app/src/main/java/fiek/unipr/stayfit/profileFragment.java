7a6ae55408f267ad1fa2c7d10161625bbc3832dc
Ali Tafaj <alittafaj@gmail.com>
2021-06-25 20:03:17 +0200

    Added access to the camera from profileFragment

--- app/src/main/java/fiek/unipr/stayfit/profileFragment.java
+++ app/src/main/java/fiek/unipr/stayfit/profileFragment.java
@@ -1,231 +1,139 @@
+package fiek.unipr.stayfit;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+import static androidx.core.content.ContextCompat.checkSelfPermission;
+
+public class profileFragment extends Fragment {
+    Button captureBtn;
+    ImageView iv_capture;
+    View view;
+    TextView tvProfileName;
+
+    private static final int Image_Capture_Code = 1;
+
+    public profileFragment() {
+        // Required empty public constructor
+    }
+
+    public static profileFragment newInstance(String param1, String param2) {
+        profileFragment fragment = new profileFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        captureBtn = (Button)view.findViewById(R.id.captureBtn);
+        //iv_capture = getActivity().findViewById(R.id.iv_capture);
+        iv_capture = (ImageView)view.findViewById(R.id.iv_capture);
+
+        Bundle args = getArguments();
+        try {
+            String myString = args.getString("email");
+            tvProfileName.setText(myString);
+
+        }
+        catch (Exception e){
+            Toast.makeText(getActivity(),"empty arg", Toast.LENGTH_LONG).show();
+            //askCameraPremission();
+        }
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cInt,Image_Capture_Code);
+                //Toast.makeText(getActivity(), "Camera button is clicked", Toast.LENGTH_LONG).show();
+                //askCameraPremission();
+            }
+        });
+
+        return view;
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Image_Capture_Code) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                iv_capture.setImageBitmap(bp);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 /*
-package fiek.unipr.stayfit;
+    private void askCameraPremission() {
+        if(ContextCompat.checkSelfPermission(this.getActivity(),Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this.getActivity(),new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import static android.media.MediaRecorder.VideoSource.CAMERA;
-import static androidx.core.content.ContextCompat.checkSelfPermission;
-
-public class profileFragment extends Fragment {
-    Button captureBtn;
-    ImageView iv_capture;
-    View view;
-
-    public profileFragment() {
-        // Required empty public constructor
-    }
-
-    public static profileFragment newInstance(String param1, String param2) {
-        profileFragment fragment = new profileFragment();
-        Bundle args = new Bundle();
-
-        return fragment;
+        }else {
+            openCamera();
+        }
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull  int[] grantResults) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        //
-        view = inflater.inflate(R.layout.fragment_profile, container, false);
-        captureBtn = (Button) view.findViewById(R.id.captureBtn);
-        iv_capture = getActivity().findViewById(R.id.iv_capture);
-
-        captureBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_LONG).show();
+        if(requestCode == CAMERA_PERM_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openCamera();
+            }else {
+                Toast.makeText(getActivity(), "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
             }
-        });
-
-        return view;
-
-    }
-
-}*//*
-
-package fiek.unipr.stayfit;
-
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import static android.media.MediaRecorder.VideoSource.CAMERA;
-import static androidx.core.content.ContextCompat.checkSelfPermission;
-
-public class profileFragment extends Fragment {
-    Button captureBtn;
-    ImageView iv_capture;
-    View view;
-    TextView tvProfileName;
-
-    public profileFragment() {
-        // Required empty public constructor
-    }
-
-    public static profileFragment newInstance(String param1, String param2) {
-        profileFragment fragment = new profileFragment();
-        Bundle args = new Bundle();
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        //
-        view = inflater.inflate(R.layout.fragment_profile, container, false);
-        captureBtn = (Button) view.findViewById(R.id.captureBtn);
-        iv_capture = getActivity().findViewById(R.id.iv_capture);
-
-        Bundle args = getArguments();
-        try {
-            String myString = args.getString("email");
-            tvProfileName.setText(myString);
-
         }
-        catch (Exception e){
-            Toast.makeText(getActivity(),"empty arg", Toast.LENGTH_LONG).show();
-        }
-
-        captureBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        return view;
-
     }
 
-}
-*/
-package fiek.unipr.stayfit;
+    private void openCamera() {
+        Toast.makeText(getActivity(), "Camera open request", Toast.LENGTH_SHORT).show();
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import static android.media.MediaRecorder.VideoSource.CAMERA;
-import static androidx.core.content.ContextCompat.checkSelfPermission;
-
-public class profileFragment extends Fragment {
-    Button captureBtn;
-    ImageView iv_capture;
-    View view;
-    TextView tvProfileName;
-
-    public profileFragment() {
-        // Required empty public constructor
-    }
-
-    public static profileFragment newInstance(String param1, String param2) {
-        profileFragment fragment = new profileFragment();
-        Bundle args = new Bundle();
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        //
-        view = inflater.inflate(R.layout.fragment_profile, container, false);
-        captureBtn = (Button) view.findViewById(R.id.captureBtn);
-        iv_capture = getActivity().findViewById(R.id.iv_capture);
-
-        Bundle args = getArguments();
-        try {
-            String myString = args.getString("email");
-            tvProfileName.setText(myString);
-
-        }
-        catch (Exception e){
-            Toast.makeText(getActivity(),"empty arg", Toast.LENGTH_LONG).show();
-        }
-
-        captureBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        return view;
-
-    }
+    }*/
 
 }
