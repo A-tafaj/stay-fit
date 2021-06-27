8d4b60284b0ddc27097a294f29293a37c137a9bf
Ali Tafaj <alittafaj@gmail.com>
2021-06-27 21:15:22 +0200

    Project now supports Firebase read and write data, recyclerView, but the program has multiple bugs so it needs a full refactory!

--- app/src/main/java/fiek/unipr/stayfit/UserActivity.java
+++ app/src/main/java/fiek/unipr/stayfit/UserActivity.java
@@ -1,35 +1,64 @@
 package fiek.unipr.stayfit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class UserActivity extends AppCompatActivity {
-    Button btnSubmit;
-    Animation scale;
+
+
+    private EditText etWeek , etWeight;
+    private Button button , button2 ;
+
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference().child("Users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        btnSubmit = findViewById(R.id.btnSubmit);
+        etWeek = findViewById(R.id.etWeek);
+        etWeight = findViewById(R.id.etWeight);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
 
-        btnSubmit.setAlpha(0f);
-
-        btnSubmit.animate().alpha(1f).setDuration(2000);
-
-        //
-        scale = AnimationUtils.loadAnimation(this, R.anim.scal_button_animation);
-        btnSubmit = (Button) findViewById(R.id.btnSubmit);
-
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                btnSubmit.startAnimation(scale);
+            public void onClick(View v) {
+                String week = etWeek.getText().toString();
+                String weight = etWeight.getText().toString();
+
+                HashMap<String, String> userMap = new HashMap<>();
+
+                userMap.put("week", week);
+                userMap.put("weight", weight);
+
+                root.push().setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(UserActivity.this, "Data Saved", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserActivity.this, ShowActivity.class));
             }
         });
     }
